e7de8b1a8aadd7b948db00e11a3ac89175c6cc42
DanielExperis <daniel.martinez.perez@experis.es>
2024-07-10 16:35:23 +0000

    Update 18:34 20240710

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("daniexamples.invoices.controller.MainView", {
-        onInit: function () {
-            const oJSONModel = new sap.ui.model.json.JSONModel();
-            const oView = this.getView();
-            oJSONModel.loadData("./model/SelectionScreenMenu.json");
-            oView.setModel(oJSONModel, "selectionScreen")
-        }
+        return Controller.extend("daniexamples.invoices.controller.MainView", {
+
+            onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen")
+            },
+
+            onFilter: function(oEvent) {
+
+            },
+            
+            onClearFilters: function() {
+                const oModelSelectionScreen = this.getView().getModel("selectionScreen");
+                oModelSelectionScreen.setProperty("/CountryKey", "");
+                oModelSelectionScreen.setProperty("/ShipName", "");
+            }
+        });
     });
-});
